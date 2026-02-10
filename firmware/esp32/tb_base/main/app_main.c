55e7220829adc28c48b14d5eaea23f2b49745920
hshaon <hshaon@gmail.com>
2026-02-10 14:18:56 -0500

    reset reason on OLED

--- firmware/esp32/tb_base/main/app_main.c
+++ firmware/esp32/tb_base/main/app_main.c
@@ -10,15 +10,41 @@
 #include "tb_ir.h"
 #include "tb_tof.h"
 #include "tb_lidar.h"
+#include "esp_system.h"
 
 // forward declare your TCP server task from your file
 void tcp_server_task(void *arg);
 
 static const char *TAG = "tb_main";
 
+static const char *reset_reason_str(esp_reset_reason_t r)
+{
+  switch (r) {
+    case ESP_RST_UNKNOWN:    return "UNKNOWN";
+    case ESP_RST_POWERON:    return "POWERON";
+    case ESP_RST_EXT:        return "EXT";
+    case ESP_RST_SW:         return "SW";
+    case ESP_RST_PANIC:      return "PANIC";
+    case ESP_RST_INT_WDT:    return "INT_WDT";
+    case ESP_RST_TASK_WDT:   return "TASK_WDT";
+    case ESP_RST_WDT:        return "WDT";
+    case ESP_RST_DEEPSLEEP:  return "DEEPSLEEP";
+    case ESP_RST_BROWNOUT:   return "BROWNOUT";
+    case ESP_RST_SDIO:       return "SDIO";
+    case ESP_RST_USB:        return "USB";
+    case ESP_RST_JTAG:       return "JTAG";
+    case ESP_RST_EFUSE:      return "EFUSE";
+    case ESP_RST_PWR_GLITCH: return "PWR_GLITCH";
+    case ESP_RST_CPU_LOCKUP: return "CPU_LOCKUP";
+    default:                 return "UNK";
+  }
+}
+
 void app_main(void)
 {
   ESP_LOGI(TAG, "boot");
+  const esp_reset_reason_t reset_reason = esp_reset_reason();
+  ESP_LOGI(TAG, "reset_reason=%d", (int)reset_reason);
 
   if (tb_wifi_init_and_connect() != ESP_OK) {
     ESP_LOGE(TAG, "wifi connect failed; rebooting in 3s");
@@ -34,6 +60,9 @@ void app_main(void)
 
   if (tb_oled_init(tb_i2c_get_bus())) {
     tb_oled_show_status("---", CONFIG_TB_WIFI_SSID, tb_wifi_get_ip_str());
+    char line[22];
+    snprintf(line, sizeof(line), "RST: %s", reset_reason_str(reset_reason));
+    tb_oled_write_line(3, line);
   }
 
   tb_motors_init();
